1d5fc0c0189245f60cb8c14adceeccc93a40bfdb
seba-solar <seba.solar@duocuc.cl>
2023-08-21 17:43:50 -0400

    Ahora tiene imagen

--- scripts.js
+++ scripts.js
@@ -7,19 +7,21 @@ function getApi(APIURL){
 }
 
 getApi(APIURL);
-    
+
 function show(data){
     let tab = 
     `<tr>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Descripcion</th>
+        <th id="imagen-tabla"></th>
     </tr>`;
     for (let r of data){
         tab += `<tr>
         <th>${r.nombre}</th>
         <th>${r.precio}</th>
         <th>${r.descripcion}</th>
+        <th><img src=${r.asset} width=100px></th>
         </tr>`;
     }
     document.getElementById("products").innerHTML = tab;
